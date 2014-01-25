93d16793d7ecc1dbc1be56094fad76e6ab9fd43f
Tim Dunne <tdunne@users.noreply.github.com>
2014-01-25 17:22:32 +1100

    Update bullybutton.py

--- bullybutton.py
+++ bullybutton.py
@@ -4,15 +4,14 @@ try:
 except ImportError:
     # Python3
     import tkinter as tk
-import webbrowser
+import os
 
 root = tk.Tk()
 image = tk.PhotoImage(file='button.gif') # the button file itself, needs to be a gif (can be a static image, just save it as a .gif)
 
-def web():
-        url = 'websitehere' # insert a website to open upon button press here
-        webbrowser.open(url, new=0, autoraise=True)
-button = tk.Button(root, command=web, image=image, bd=0, height=62, width=117)
+def button():
+    os.startfile("mail.exe")
+button = tk.Button(root, command=button, image=image, bd=0, height=62, width=117)
 button.grid(row=2, column=2)
 
 root.mainloop()
